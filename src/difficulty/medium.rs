8df3db4f3c877a76b3362b1da9fa0912ba68e821
Luca Pruneti <luca.pruneti@gmail.com>
2022-02-12 14:33:11 +0100

    Added add_two_numbers challange

--- src/difficulty/medium.rs
+++ src/difficulty/medium.rs
@@ -31,16 +31,18 @@ impl ListNode {
     fn new(val: i32) -> Self {
         ListNode { next: None, val }
     }
+}
 
+impl ListNode {
     fn add_next(&mut self, val: i32) {
-        self.next = ListNode::wrap(val);
+        self.next = ListNode::create_from_value(val);
     }
 
-    fn wrap(val: i32) -> Option<Box<ListNode>> {
+    fn create_from_value(val: i32) -> Option<Box<ListNode>> {
         Some(Box::new(ListNode::new(val)))
     }
 
-    fn new_from_vect(vals: Vec<i32>) -> Self {
+    fn create_from_vect(vals: Vec<i32>) -> Option<Box<ListNode>> {
         if vals.is_empty() {
             panic!("Input vector cannot be empty");
         }
@@ -48,10 +50,10 @@ impl ListNode {
         let mut tail = &mut head;
         for i in 1..vals.len() {
             tail.add_next(vals[i]);
-            tail = Option::as_deref_mut(&mut tail.next).unwrap();
+            tail = tail.next.as_deref_mut().unwrap();
         }
 
-        head
+        Some(Box::new(head))
     }
 }
 
@@ -59,7 +61,60 @@ pub fn add_two_numbers(
     l1: Option<Box<ListNode>>,
     l2: Option<Box<ListNode>>,
 ) -> Option<Box<ListNode>> {
-    todo!()
+    let mut result_head = ListNode::new(0);
+    let mut result_tail = &mut result_head;
+
+    let mut a1 = &l1;
+    let mut a2 = &l2;
+
+    let mut remainder = 0;
+    loop {
+        let addend1 = match a1 {
+            Some(v) => {
+                a1 = &a1.as_deref().unwrap().next;
+                v.as_ref().val
+            }
+            _ => 0,
+        };
+
+        let addend2 = match a2 {
+            Some(v) => {
+                a2 = &a2.as_deref().unwrap().next;
+                v.as_ref().val
+            }
+            _ => 0,
+        };
+
+        let partial_sum = add_addends(addend1, addend2, remainder);
+
+        result_tail.add_next(partial_sum.0);
+        result_tail = result_tail.next.as_deref_mut().unwrap();
+
+        remainder = partial_sum.1;
+
+        if a1.is_none() && a2.is_none() {
+            if remainder > 0 {
+                result_tail.add_next(remainder);
+            }
+            break;
+        }
+    }
+
+    result_head.next
+}
+
+/// Used to implement the partial sum of two number plus the remainder of a previous one
+/// Return a tuple containing the unit part and the remainder for the next addiction
+fn add_addends(addend1: i32, addend2: i32, remainder: i32) -> (i32, i32) {
+    assert!(addend1 >= 0 && addend1 <= 9);
+    assert!(addend2 >= 0 && addend2 <= 9);
+    assert!(remainder >= 0 && remainder <= 9);
+
+    let tmp_sum = addend1 + addend2 + remainder;
+    let res = tmp_sum % 10; // extract the unit part
+    let rem = (tmp_sum - res) % 9; // extract the ten part
+
+    (res, rem)
 }
 
 #[cfg(test)]
@@ -70,18 +125,62 @@ mod tests {
     // -----------------------
     // 2. Add Two Numbers
     // -----------------------
+    #[test]
+    fn test_add_addends_with_no_input_remainder_no_out_remainder() {
+        let result = add_addends(4, 5, 0);
+        assert_eq!(result, (9, 0))
+    }
+
+    #[test]
+    fn test_add_addends_with_in_remainder_no_out_remainder() {
+        let result = add_addends(3, 2, 1);
+        assert_eq!(result, (6, 0))
+    }
+
+    #[test]
+    fn test_add_addends_with_in_remainder_and_out_remainder() {
+        let result = add_addends(6, 5, 1);
+        assert_eq!(result, (2, 1))
+    }
+
+    #[test]
+    fn test_add_addends_with_in_remainder_and_out_remainder_max() {
+        let result = add_addends(9, 9, 9);
+        assert_eq!(result, (7, 2))
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_add_addend_with_two_digits_addend1() {
+        add_addends(10, 9, 9);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_add_addend_with_two_digits_addend2() {
+        add_addends(9, 10, 9);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_add_addend_with_two_digits_remainder() {
+        add_addends(9, 9, 10);
+    }
 
     #[test]
     #[should_panic]
     fn test_list_node_new_from_empty_vect_should_panic() {
         let vals: Vec<i32> = Vec::new();
-        ListNode::new_from_vect(vals);
+        ListNode::create_from_vect(vals);
     }
 
     #[test]
     fn test_list_node_new_from_vect() {
         let vals = vec![2, 4, 3];
-        let list_node = ListNode::new_from_vect(vals);
+        let list_nodes = ListNode::create_from_vect(vals);
+
+        let list_node = list_nodes.unwrap();
+
         assert_eq!(list_node.val, 2);
         assert!(list_node.next.is_some());
 
@@ -97,29 +196,36 @@ mod tests {
     #[test]
     fn test_add_two_numbers_example_1() {
         // Input: l1 = [2,4,3], l2 = [5,6,4]
+        let l1 = ListNode::create_from_vect(vec![2, 4, 3]);
+        let l2 = ListNode::create_from_vect(vec![5, 6, 4]);
+
         // Output: [7,0,8]
-        // Explanation: 342 + 465 = 807.
-
-        // let l1 = Some(
-        //     Box::new(ListNode::node(
-        //     2,
-        //     Some(Box::new(ListNode::new(4, Some(Box::new(ListNode::new(3)))))),
-        // )));
-
-        // let b1 = Some(Box::new(ListNode::new(5)));
-        // let b2 = Some(Box::new(ListNode::new(6)));
-        // let b3 = Some(Box::new(ListNode::new(4)));
+        let result = add_two_numbers(l1, l2);
+        assert_eq!(result, ListNode::create_from_vect(vec![7, 0, 8]));
     }
 
     #[test]
     fn test_add_two_numbers_example_2() {
         // Input: l1 = [0], l2 = [0]
+        let l1 = ListNode::create_from_vect(vec![0]);
+        let l2 = ListNode::create_from_vect(vec![0]);
+
         // Output: [0]
+        let result = add_two_numbers(l1, l2);
+        assert_eq!(result, ListNode::create_from_vect(vec![0]));
     }
 
     #[test]
     fn test_add_two_numbers_example_3() {
         // Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+        let l1 = ListNode::create_from_vect(vec![9, 9, 9, 9, 9, 9, 9]);
+        let l2 = ListNode::create_from_vect(vec![9, 9, 9, 9]);
+
         // Output: [8,9,9,9,0,0,0,1]
+        let result = add_two_numbers(l1, l2);
+        assert_eq!(
+            result,
+            ListNode::create_from_vect(vec![8, 9, 9, 9, 0, 0, 0, 1])
+        );
     }
 }
